fd567c384a602f73cf9908190a61da0ef07c9317
sergioxd09 <sergio9junio@gmail.com>
2023-05-08 19:40:56 -0500

    initial commit

---
+++ src/main/java/pe/edu/cibertec/appCineCibertec/AppCineCibertecApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appCineCibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCineCibertecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCineCibertecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/appCineCibertec/AppCineCibertecApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appCineCibertec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCineCibertecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
